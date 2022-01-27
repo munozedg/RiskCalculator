5ad7db0577acfb1bd4dcbfad2bf50fbe9eca4b3e
munozedg <munozedg@hotmail.com>
2022-01-27 14:00:07 -0600

    upd

--- RiskCalculator/server.R
+++ RiskCalculator/server.R
@@ -12,20 +12,17 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
+  output$distPlot <- renderPlot({
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        # hist(x, breaks = bins, col = 'darkgray', border = 'white')
-        ggplot(
-          data = dat1) +
-          geom_line(
-            aes(x=reporting_date, y=total_case_daily_change)
-          )
+    # draw the histogram with the specified number of bins
+    # hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    ggplot(data = dat1) +
+      geom_line(aes(x = reporting_date, y = total_case_daily_change))
 
-    })
+  })
 
 })
