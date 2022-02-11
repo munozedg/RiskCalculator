b33e6bc07d2bba560f14d1b9f54b4456d06aa191
Edgar Munoz <munozedg@users.noreply.github.com>
2022-02-11 04:38:39 -0600

    Update modif_mins.R

--- functions/modif_mins.R
+++ functions/modif_mins.R
@@ -1,11 +1,11 @@
 modif_mins <- function(myfilename, units = "mins") {
-  # clac how old is this file in mins
+  # calc how old is this file in mins
   hora_actual <- as.POSIXct(Sys.time())
   if (file.exists(myfilename)) {
     hora_modif <- as.POSIXct( file.info(myfilename)$mtime )
     modif_hace_mins <- difftime(hora_actual, hora_modif, units = units)
   } else {
-    modif_hace_mins <- -1 # difftime(hora_actual, hora_actual, units = units)
+    modif_hace_mins <- -1 
   }
   modif_hace_mins
 }
